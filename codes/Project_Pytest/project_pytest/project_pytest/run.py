34411406a059c929098d7aed9c7469383177a03a
Bruno Barbosa Santos <bruno-bbs80@hotmail.com>
2023-02-23 20:35:26 -0300

    update run.py

--- codes/Project_Pytest/project_pytest/project_pytest/run.py
+++ codes/Project_Pytest/project_pytest/project_pytest/run.py
@@ -1,18 +1,12 @@
 from main import CepRegister
-from FileManagement import get_file_path
-#cep_address = CepRegister().get_cep_data("08150545")
-# cep_address = CepRegister().get_random_character()
 
-#cep_address = CepRegister().check_cep_file("08150545")
+cep_address = CepRegister().get_cep_data("08150545")
+print(cep_address)
+#cep_check = CepRegister().check_cep_file("08150545")
 
 #cep_address = CepRegister().delete_cep_file("08150545")
 
 # cep_address = CepRegister().get_cep_data("08150545")
-
 # cep_file = CepRegister().create_cep_file(cep_address)
 #print(cep_file)
-#print(cep_address)
-#print(type(cep_address))
-#print(type(cep_address))
-teste = get_file_path("juca")
-print(teste)
+
